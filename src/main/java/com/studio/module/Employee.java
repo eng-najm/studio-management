16b6108325de64b80a023b079d2a692d730e56e1
najm <najm113nagm@gmail.com>
2026-05-29 16:57:27 +0300

    feat: add userName and userPassword fields to Employee class with corresponding getters and setters

--- src/main/java/com/studio/module/Employee.java
+++ src/main/java/com/studio/module/Employee.java
@@ -9,21 +9,25 @@ enum Role {
 public class Employee extends Person {
     private int salary;
     private Role role;
+    private String userName;
+    private String userPassword;
 
     public Employee(int id, String firstName, String meddilName, String lastName, char sex, String phone,
-            String address, Date hireDate, int salary, Role role) {
+            String address, Date hireDate, int salary, Role role, String userName, String userPassword) {
         super(id, firstName, meddilName, lastName, sex, phone, address, hireDate);
-        this.role = role;
         this.salary = salary;
-
+        this.role = role;
+        this.userName = userName;
+        this.userPassword = userPassword;
     }
 
     public Employee(String firstName, String meddilName, String lastName, char sex, String phone, String address,
-            int salary, Role role) {
+            int salary, Role role, String userName, String userPassword) {
         super(firstName, meddilName, lastName, sex, phone, address);
         this.salary = salary;
         this.role = role;
-
+        this.userName = userName;
+        this.userPassword = userPassword;
     }
 
     public int getSalary() {
@@ -40,6 +44,22 @@ public class Employee extends Person {
 
     public void setRole(Role role) {
         this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
     }
 
 }
