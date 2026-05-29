d6a58d956bbd07e531aa617af65c734877642a1f
najm <najm113nagm@gmail.com>
2026-05-29 20:06:55 +0300

    refactor: make connection method static and move dotenv initialization inside

--- src/main/java/com/studio/core/DBHelper.java
+++ src/main/java/com/studio/core/DBHelper.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class DBHelper {
-    Dotenv dotenv = Dotenv.load();
 
-    public Connection connection() throws SQLException {
+    public static Connection connection() throws SQLException {
+        Dotenv dotenv = Dotenv.load();
         try {
             return DriverManager.getConnection(dotenv.get("DB_URL"), dotenv.get("DB_USER"), dotenv.get("DB_PASSWORD"));
         } catch (SQLException e) {
