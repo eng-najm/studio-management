eba2dc78fdc84fe19cb71c8edfeb07de4964131e
najm <najm113nagm@gmail.com>
2026-05-31 18:36:58 +0300

    feat: add EmployeePage and update Main to include employee management route

--- src/main/java/com/studio/core/constants/AppRoutes.java
+++ src/main/java/com/studio/core/constants/AppRoutes.java
@@ -3,4 +3,6 @@ package com.studio.core.constants;
 public class AppRoutes {
     public final static String LOGIN = "LOGIN";
     public final static String DASHBOARD = "DASHBOARD";
+    public final static String EMPLOYEE_MANAGEMENT = "EMPLOYEE_MANAGEMENT";
+
 }
---
+++ src/main/java/com/studio/features/employee_management/view/EmployeePage.java
@@ -0,0 +1,10 @@
+package com.studio.features.employee_management.view;
+
+import javax.swing.JPanel;
+
+public class EmployeePage extends JPanel {
+    public EmployeePage() {
+        this.setSize(1000, 1000);
+    }
+
+}
