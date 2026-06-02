3b5eeb786d0007112ee06d1c1a65e2fb2ad42747
najm <najm113nagm@gmail.com>
2026-06-02 16:50:21 +0300

    feat: update DashboardPage to integrate Employee Management and improve navigation

--- src/main/java/com/studio/features/dashboard/view/DashboardPage.java
+++ src/main/java/com/studio/features/dashboard/view/DashboardPage.java
@@ -1,8 +1,10 @@
 package com.studio.features.dashboard.view;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
@@ -10,27 +12,41 @@ import javax.swing.JPanel;
 import com.studio.core.constants.AppRoutes;
 import com.studio.core.shared_widgets.AppButton;
 import com.studio.features.dashboard.model.DashItem;
+import com.studio.features.employee_management.controller.EmployeeController;
+import com.studio.features.employee_management.view.EmployeePage;
 
 public class DashboardPage extends JPanel {
     DashItem[] dashItems = {
-            new DashItem("Employee Management", "", AppRoutes.DASHBOARD),
+            new DashItem("Employee Management", "", AppRoutes.EMPLOYEE_MANAGEMENT),
             new DashItem("Customer Management", "", ""),
             new DashItem("Order Management", "", ""),
-            new DashItem("LogOut", "", "")
     };
+    CardLayout cardLayout = new CardLayout();
+    JPanel container = new JPanel(cardLayout);
+    EmployeePage employeePage = new EmployeePage();
 
     public DashboardPage() {
-
+        new EmployeeController(employeePage);
         this.setLayout(new BorderLayout());
+        //
         JPanel items = new JPanel();
+        items.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
         items.setLayout(new BoxLayout(items, BoxLayout.Y_AXIS));
         items.setSize(150, HEIGHT);
         for (DashItem item : dashItems) {
-            items.add(new AppButton(item.getName()));
+            AppButton button = new AppButton(item.getName());
+            button.addActionListener(e -> goTo(item.getRoute()));
+            items.add(button);
             items.add(Box.createRigidArea(new Dimension(0, 30)));
         }
+        //
         this.add(items, BorderLayout.WEST);
+        this.add(container, BorderLayout.CENTER);
+        container.add(employeePage, AppRoutes.EMPLOYEE_MANAGEMENT);
 
     }
 
+    public void goTo(String route) {
+        cardLayout.show(container, route);
+    }
 }
