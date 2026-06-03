7faa46343d60eb70e36ef733cebf48738a895555
najm <najm113nagm@gmail.com>
2026-06-03 22:05:39 +0300

    feat: add custom font support and apply FlatLightLaf for improved UI aesthetics

--- src/main/java/com/studio/Main.java
+++ src/main/java/com/studio/Main.java
@@ -1,11 +1,15 @@
 package com.studio;
 
 import java.awt.CardLayout;
+import java.awt.Font;
+import java.io.File;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import com.studio.Main;
 import com.studio.core.constants.AppRoutes;
 import com.studio.features.dashboard.view.DashboardPage;
@@ -19,6 +23,15 @@ public class Main extends JFrame {
     JPanel container = new JPanel(cardLayout);
 
     public Main() {
+        FlatLightLaf.setup();
+        try {
+            File fontFile = new File("src/main/java/com/studio/assets/fontBold.otf");
+            Font newFont = Font.createFont(Font.TRUETYPE_FONT, fontFile).deriveFont(25f);
+            UIManager.put("defaultFont", newFont);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
         setTitle("App");
         setSize(500, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
