7d5e6195da348a179a5fce117341cbb677d2338c
najm <najm113nagm@gmail.com>
2026-06-05 22:46:02 +0300

    feat: add EDITE_EMPLOYEE route to AppRoutes for employee editing functionality

--- src/main/java/com/studio/core/constants/AppRoutes.java
+++ src/main/java/com/studio/core/constants/AppRoutes.java
@@ -4,5 +4,6 @@ public class AppRoutes {
     public final static String LOGIN = "LOGIN";
     public final static String DASHBOARD = "DASHBOARD";
     public final static String EMPLOYEE_MANAGEMENT = "EMPLOYEE_MANAGEMENT";
+    public final static String EDITE_EMPLOYEE = "EDITE_EMPLOYEE";
 
 }
