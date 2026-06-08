5b1b254ada642d0e4c339ce92c626d3d2a0613fd
najm <najm113nagm@gmail.com>
2026-06-08 16:54:47 +0300

    feat: add routes for coupon and skill management in AppRoutes

--- src/main/java/com/studio/core/constants/AppRoutes.java
+++ src/main/java/com/studio/core/constants/AppRoutes.java
@@ -7,5 +7,9 @@ public class AppRoutes {
     public final static String EDITE_EMPLOYEE = "EDITE_EMPLOYEE";
     public final static String CUSTOMER_MANAGEMENT = "CUSTOMER_MANAGEMENT";
     public final static String EDIT_CUSTOMER = "EDIT_CUSTOMER";
+    public final static String COUPON_MANAGEMENT = "COUPON_MANAGEMENT";
+    public final static String EDIT_COUPON = "EDIT_COUPON";
+    public final static String SKILL_MANAGEMENT = "SKILL_MANAGEMENT";
+    public final static String EDIT_SKILL = "EDIT_SKILL";
 
 }
